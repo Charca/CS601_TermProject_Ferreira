c751ea20e2271f26a2e1fd893607a91b5bc29a58
Maxi Ferreira <charca@gmail.com>
2021-12-12 08:44:59 -0800

    Added alert to contact form

--- js/contact.js
+++ js/contact.js
@@ -5,15 +5,32 @@
   function handleSubmit(event) {
     event.preventDefault()
     const formData = new FormData(contactForm);
+    const formAlert = document.getElementById('form-alert');
+
+    // Hide the alert message when submitting the form
+    formAlert.innerHTML = "";
+    formAlert.classList.add('hidden');
+    formAlert.classList.remove('alert-success');
+    formAlert.classList.remove('alert-danger');
 
     fetch('/', {
       method: 'POST',
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     }).then(() => {
-      alert('Thank you for your message!')
+      console.log('Message sent successfully');
+      // clear the form
+      contactForm.reset();
+      // show succes message
+      formAlert.classList.remove('hidden');
+      formAlert.classList.add('alert-success');
+      formAlert.innerHTML = 'Message sent successfully';
     }).catch((error) => {
-      alert(error);
+      console.log(error);
+      // show error message
+      formAlert.classList.remove('hidden');
+      formAlert.classList.add('alert-error');
+      formAlert.innerHTML = 'Message could not be sent. Please try again later.';
     });
   }
 })();
